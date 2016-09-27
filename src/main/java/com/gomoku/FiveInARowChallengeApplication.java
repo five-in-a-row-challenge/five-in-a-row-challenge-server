4139decbd55ade56fbaf207086d164cea58eacba
DanielZelei <zelei.dan@gmail.com>
2016-09-27 23:10:28 +0200

    init five in a row (gomoku) project

---
+++ src/main/java/com/gomoku/FiveInARowChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.gomoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveInARowChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveInARowChallengeApplication.class, args);
+	}
+}
